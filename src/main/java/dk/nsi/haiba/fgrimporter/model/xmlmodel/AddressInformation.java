54365e25157af9257a5c3f21c44fc943bee036cf
Aksel Schmidt <aks@trifork.com>
2014-01-20 11:30:32 +0100

    copied from https://fisheye.nspop.dk/browse/public/components/sdm4-sorimporter/latest, rev. 439

--- src/main/java/dk/nsi/haiba/fgrimporter/model/xmlmodel/AddressInformation.java
+++ src/main/java/dk/nsi/haiba/fgrimporter/model/xmlmodel/AddressInformation.java
@@ -25,132 +25,132 @@
  * SOFTWARE.
  */
 
-package dk.nsi.haiba.fgrimporter.model.xmlmodel;
+package dk.nsi.sdm4.sor.xmlmodel;
 
 public class AddressInformation
 {
-	private String streetName;
-	private String streetBuildingIdentifier;
-	private String postCodeIdentifier;
-	private String districtName;
-	private String countryIdentificationCode;
+    private String streetName;
+    private String streetBuildingIdentifier;
+    private String postCodeIdentifier;
+    private String districtName;
+    private String countryIdentificationCode;
 
-	private String emailAddressIdentifier;
-	private String website;
-	private String telephoneNumberIdentifier;
-	private String faxNumberIdentifier;
+    private String emailAddressIdentifier;
+    private String website;
+    private String telephoneNumberIdentifier;
+    private String faxNumberIdentifier;
 
-	private Long eanLocationCode;
-	private Boolean entityInheritedIndicator;
+    private Long eanLocationCode;
+    private Boolean entityInheritanceIndicator;
 
-	public String getStreetName()
-	{
-		return streetName;
-	}
+    public String getStreetName()
+    {
+        return streetName;
+    }
 
-	public void setStreetName(String streetName)
-	{
-		this.streetName = streetName;
-	}
+    public void setStreetName(String streetName)
+    {
+        this.streetName = streetName;
+    }
 
-	public String getStreetBuildingIdentifier()
-	{
-		return streetBuildingIdentifier;
-	}
+    public String getStreetBuildingIdentifier()
+    {
+        return streetBuildingIdentifier;
+    }
 
-	public void setStreetBuildingIdentifier(String streetBuildingIdentifier)
-	{
-		this.streetBuildingIdentifier = streetBuildingIdentifier;
-	}
+    public void setStreetBuildingIdentifier(String streetBuildingIdentifier)
+    {
+        this.streetBuildingIdentifier = streetBuildingIdentifier;
+    }
 
-	public String getPostCodeIdentifier()
-	{
-		return postCodeIdentifier;
-	}
+    public String getPostCodeIdentifier()
+    {
+        return postCodeIdentifier;
+    }
 
-	public void setPostCodeIdentifier(String postCodeIdentifier)
-	{
-		this.postCodeIdentifier = postCodeIdentifier;
-	}
+    public void setPostCodeIdentifier(String postCodeIdentifier)
+    {
+        this.postCodeIdentifier = postCodeIdentifier;
+    }
 
-	public String getDistrictName()
-	{
-		return districtName;
-	}
+    public String getDistrictName()
+    {
+        return districtName;
+    }
 
-	public void setDistrictName(String districtName)
-	{
-		this.districtName = districtName;
-	}
+    public void setDistrictName(String districtName)
+    {
+        this.districtName = districtName;
+    }
 
-	public String getCountryIdentificationCode()
-	{
-		return countryIdentificationCode;
-	}
+    public String getCountryIdentificationCode()
+    {
+        return countryIdentificationCode;
+    }
 
-	public void setCountryIdentificationCode(String countryIdentificationCode)
-	{
-		this.countryIdentificationCode = countryIdentificationCode;
-	}
+    public void setCountryIdentificationCode(String countryIdentificationCode)
+    {
+        this.countryIdentificationCode = countryIdentificationCode;
+    }
 
-	public String getEmailAddressIdentifier()
-	{
-		return emailAddressIdentifier;
-	}
+    public String getEmailAddressIdentifier()
+    {
+        return emailAddressIdentifier;
+    }
 
-	public void setEmailAddressIdentifier(String emailAddressIdentifier)
-	{
-		this.emailAddressIdentifier = emailAddressIdentifier;
-	}
+    public void setEmailAddressIdentifier(String emailAddressIdentifier)
+    {
+        this.emailAddressIdentifier = emailAddressIdentifier;
+    }
 
-	public String getWebsite()
-	{
-		return website;
-	}
+    public String getWebsite()
+    {
+        return website;
+    }
 
-	public void setWebsite(String website)
-	{
-		this.website = website;
-	}
+    public void setWebsite(String website)
+    {
+        this.website = website;
+    }
 
-	public String getTelephoneNumberIdentifier()
-	{
-		return telephoneNumberIdentifier;
-	}
+    public String getTelephoneNumberIdentifier()
+    {
+        return telephoneNumberIdentifier;
+    }
 
-	public void setTelephoneNumberIdentifier(String telephoneNumberIdentifier)
-	{
-		this.telephoneNumberIdentifier = telephoneNumberIdentifier;
-	}
+    public void setTelephoneNumberIdentifier(String telephoneNumberIdentifier)
+    {
+        this.telephoneNumberIdentifier = telephoneNumberIdentifier;
+    }
 
-	public String getFaxNumberIdentifier()
-	{
-		return faxNumberIdentifier;
-	}
+    public String getFaxNumberIdentifier()
+    {
+        return faxNumberIdentifier;
+    }
 
-	public void setFaxNumberIdentifier(String faxNumberIdentifier)
-	{
-		this.faxNumberIdentifier = faxNumberIdentifier;
-	}
+    public void setFaxNumberIdentifier(String faxNumberIdentifier)
+    {
+        this.faxNumberIdentifier = faxNumberIdentifier;
+    }
 
-	public Long getEanLocationCode()
-	{
-		return eanLocationCode;
-	}
+    public Long getEanLocationCode()
+    {
+        return eanLocationCode;
+    }
 
-	public void setEanLocationCode(Long eanLocationCode)
-	{
-		this.eanLocationCode = eanLocationCode;
-	}
+    public void setEanLocationCode(Long eanLocationCode)
+    {
+        this.eanLocationCode = eanLocationCode;
+    }
 
-	public Boolean isEntityInheritedIndicator()
-	{
-		return entityInheritedIndicator;
-	}
+    public Boolean hasEntityInheritanceIndicator()
+    {
+        return entityInheritanceIndicator;
+    }
 
-	public void setEntityInheritedIndicator(Boolean entityInheritedIndicator)
-	{
-		this.entityInheritedIndicator = entityInheritedIndicator;
-	}
+    public void setEntityInheritanceIndicator(Boolean entityInheritanceIndicator)
+    {
+        this.entityInheritanceIndicator = entityInheritanceIndicator;
+    }
 
 }
