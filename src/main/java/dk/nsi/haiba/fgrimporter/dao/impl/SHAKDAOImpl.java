bf7b5a3f7d773beadb31505ad2f5375116df5d85
Aksel Schmidt <aks@trifork.com>
2014-01-23 12:17:34 +0100

    truncate table

--- src/main/java/dk/nsi/haiba/fgrimporter/dao/impl/SHAKDAOImpl.java
+++ src/main/java/dk/nsi/haiba/fgrimporter/dao/impl/SHAKDAOImpl.java
@@ -81,7 +81,7 @@ public class SHAKDAOImpl extends CommonDAO implements SHAKDAO {
 	@Override
 	public void clearOrganisationTable() throws DAOException {
 	    try {
-			jdbc.update("DELETE FROM Organisation");
+			jdbc.update("TRUNCATE TABLE Organisation");
         } catch (Exception e) {
             throw new DAOException("", e);
         }
